b8829b5acda70058aae944b4e61900f0a3843709
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-14 17:18:11 -0500

    create-4binary-is leaf-betty

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is leaf - check code
+ * binary_tree_is_leaf - check code
  * @node: pointer to the node to check
- * Return: 1 if node is a leaf, otherwise 0
+ * Return: 1 if node is a leaf
  */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
