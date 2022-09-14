45dbc044fdeca02f628c9157fc29b016096e0a18
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-14 14:32:24 -0500

    update-NULL-

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,8 +13,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode = binary_tree_node(parent, value);
 
+	if (newNode == NULL)
+		return (NULL);
 	if (parent == NULL)
 	{
+		parent = newNode;
 		return (NULL);
 	}
 	if (parent->left != NULL)
