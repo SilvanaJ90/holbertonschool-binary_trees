9af6ac6ab7f0ca3f08a9332bff704558136c0f36
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-14 17:26:59 -0500

    create-5-binary

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,7 +8,11 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
+	binary_tree_t *parent = NULL;
+
 	if (node == NULL)
 		return (0);
-	return (1);
+	if (node->left != parent && node->right != parent )
+		return (1);
+	return (0);
 }
