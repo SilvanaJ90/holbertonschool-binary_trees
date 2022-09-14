7b495048f666d9fc6e5d545878c1bd593d4b0330
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-14 14:29:28 -0500

    update-error-Correct output - case: Insert left child in a node that already has a left child

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,16 +18,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent = newNode;
 		return (NULL);
 	}
-	else
+	if (parent->left != NULL)
 	{
-		int root = parent->n;
-
-		if (value < root && parent->left != NULL)
-		{
-			binary_tree_insert_left(parent->left, value);
-			newNode->left = parent->left;
-			parent->left->parent = newNode;
-		}
+		newNode->left = parent->left;
+		parent->left->parent = newNode;
 	}
 	parent->left = newNode;
 	return (newNode);
