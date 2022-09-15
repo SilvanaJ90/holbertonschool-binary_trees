94c745d0a1fd8c9f3a25d1ccb8547708fce5561b
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-15 18:47:36 -0500

    update

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,6 +8,8 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (-1);
 	if (tree != NULL)
 	{
 		int lDepth = binary_tree_height(tree->left);
@@ -18,5 +20,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		else
 			return (rDepth + 1);
 	}
-	return (-1);
+	return (0);
 }
