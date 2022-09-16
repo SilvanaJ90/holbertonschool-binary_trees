35d0bd0e6896f439f486affe8d7a3d7f1dce997c
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-15 22:32:47 -0500

    create-10-depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,6 +8,15 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	int count = 0;
+
 	if (tree == NULL)
 		return (0);
+
+	while (tree->parent != NULL)
+	{
+		tree = tree->parent;
+		count++;
+	}
+	return (count);
 }
