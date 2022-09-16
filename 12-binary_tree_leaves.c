b06c8ee2909689cb0ff7cd405b25088d0c455707
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-16 07:55:05 -0500

    update-c9¡

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_leaves - check code
  * @tree: ointer to the root node of the tree to count the number of leaves
- * Return:
+ * Return: lLeaves + rLeaves or 0 failure
  *
  */
 
@@ -12,12 +12,10 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if(tree->left == NULL && tree->right ==NULL)
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	else
-	{
-		int lLeaves = binary_tree_leaves(tree->left);
-		int rLeaves = binary_tree_leaves(tree->right);
-		return ( lLeaves + rLeaves);
-	}
+	int lLeaves = binary_tree_leaves(tree->left);
+	int rLeaves = binary_tree_leaves(tree->right);
+
+	return (lLeaves + rLeaves);
 }
