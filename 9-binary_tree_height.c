bae07efd60517f11f00d091b0961f6557801b51a
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-16 08:16:06 -0500

    update

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include <sys/param.h>
 
 /**
  * binary_tree_height - check code
@@ -8,20 +9,14 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int lDepth;
-	int rDepth;
+	int lDepth = 0;
+	int rDepth = 0;
 
 	if (tree == NULL)
 		return (-1);
 
-	if (tree)
-	{
-		lDepth = binary_tree_height(tree->left);
-		rDepth = binary_tree_height(tree->right);
-		if (lDepth > rDepth)
-			return (lDepth + 1);
-		else
-			return (rDepth + 1);
-	}
+	lDepth = binary_tree_height(tree->left);
+	rDepth = binary_tree_height(tree->right);
+	return (MAX(1 + lDepth, 1 + rDepth));
 	return (0);
 }
