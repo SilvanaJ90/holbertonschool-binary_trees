9a8a2dece7f0b0146f16fb33c17a1cc0b92b3bbd
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-16 21:25:03 -0500

    create-17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,8 +8,9 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (node == NULL || parent == NULL)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
-	if (node no tinee hermanos )
-		return (NULL);
+	if (node == node->parent->left)
+		return (node->parent->right);
+	return (node->parent->left);
 }
